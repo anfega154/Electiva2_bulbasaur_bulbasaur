349b970a29e8e6e96a6028ec8a63a71e5ea608d5
andres.ganan <andres.ganan@mantum.com.co>
2024-08-03 09:14:01 -0500

    avances

--- src/infrastucture/config/server.js
+++ src/infrastucture/config/server.js
@@ -2,22 +2,20 @@ const express = require('express')
 const app = express();
 const cors=require("cors")
 require('dotenv').config();
+const routerUser = require('../../app/Routers/v1/apiRouters');
 
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-
-    res.status(200).json({
-      status: " success",
-      mensaje: "Bienvenido"
-    });
-  })
+app.use("/api",routerUser)
 
 
 const PORT = process.env.PORT || 3700;
 app.listen(PORT, (req, res) => {
   console.log("server in  http://localHost:" + PORT);
 });
+
+
+module.exports = app;
