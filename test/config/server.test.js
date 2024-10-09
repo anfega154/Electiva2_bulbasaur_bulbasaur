840f3a68336c98cb129bdcb338c80ef1f6e8bacc
andres.ganan <andres.ganan@mantum.com.co>
2024-10-08 22:29:40 -0500

    server and tweet test

--- test/config/server.test.js
+++ test/config/server.test.js
@@ -4,21 +4,22 @@ const i18n = require('../../src/Utils/helpers/i18n');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 
+// Mocks
 jest.mock('express');
 jest.mock('cors');
 jest.mock('dotenv', () => ({
-  config: jest.fn()
+  config: jest.fn(),
 }));
 jest.mock('../../src/v1/User/Routers/UserRouters', () => jest.fn());
 jest.mock('../../src/v1/Auth/Routers/ApiRouters', () => jest.fn());
 jest.mock('../../src/v1/Follow/Roters/FollowRouters', () => jest.fn());
 jest.mock('../../src/v1/Tweet/Routers/TweetRouters', () => jest.fn());
 jest.mock('../../src/Utils/helpers/i18n', () => ({
-  init: jest.fn()
+  init: jest.fn(),
 }));
 jest.mock('swagger-ui-express', () => ({
   serve: jest.fn(),
-  setup: jest.fn()
+  setup: jest.fn(),
 }));
 jest.mock('swagger-jsdoc', () => jest.fn());
 
@@ -36,23 +37,27 @@ describe('Server.js', () => {
             listen: mockListen,
         });
 
-        app = require('../../src/index');
+        jest.isolateModules(() => {
+            app = require('../../src/index');
+        });
     });
 
     describe('with initialize the express', () => {
         test('should initialize the express app and middleware', () => {
-            expect(express).toHaveBeenCalled();
-            expect(mockUse).toHaveBeenCalledWith(cors());
+            expect(express).toHaveBeenCalled(); // Asegura que express fue llamado
+            expect(mockUse).toHaveBeenCalledWith(cors()); // Verifica que se llama cors
             expect(mockUse).toHaveBeenCalledWith(express.urlencoded({ extended: true }));
             expect(mockUse).toHaveBeenCalledWith(express.json());
-            expect(mockUse).toHaveBeenCalledWith(i18n.init);
-            expect(mockUse).toHaveBeenCalledWith('/api-docs', swaggerUi.serve, swaggerUi.setup(expect.anything()));
-        });     
-    })
-    
-    describe('with listen on the correct port ', () => {
+            expect(mockUse).toHaveBeenCalledWith(i18n.init); // Verifica la inicialización de i18n
+            expect(mockUse).toHaveBeenCalledWith('/api-docs', swaggerUi.serve, swaggerUi.setup(expect.anything())); // Verifica swagger
+        });
+    });
+
+    describe('with listen on the correct port', () => {
         test('should listen on the correct port', () => {
-            expect(mockListen).toHaveBeenCalledWith(process.env.PORT || 3700, expect.any(Function));
-        });      
-    })
+            const PORT = process.env.PORT || 3700;
+
+            expect(mockListen).toHaveBeenCalledWith(PORT, expect.any(Function));
+        });
+    });
 });
